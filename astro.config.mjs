3320627035190f6fa7772d5721a452a9cf2bd802
Eloi-Perez <eloi104@hotmail.com>
2022-05-23 17:28:32 +0100

    add site config for github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Eloi-Perez.github.io/',
+    base: '/black-friday-countdown'
+});
